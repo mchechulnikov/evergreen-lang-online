fa54302c7cdcb2057f672055b86c672ad4cf3bb5
sunloving <ilfirin.nar@gmail.com>
2017-08-24 19:19:03 +0300

    simple maket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {EditorComponent} from './editor/editor.component';
+import {HelpComponent} from './help/help.component';
+import {OutputComponent} from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EditorComponent,
+    HelpComponent,
+    OutputComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ]
 })
 export class AppModule { }
