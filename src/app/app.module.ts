66a9a9ef646659ca686f21489066e740e17e38db
sunloving <ilfirin.nar@gmail.com>
2017-08-26 01:55:26 +0300

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
+import {MaterialModule} from '@angular-mdc/web';
 import {HeaderComponent} from './header/header.component';
 import {EditorComponent} from './editor/editor.component';
-import {HelpComponent} from './help/help.component';
 import {OutputComponent} from './output/output.component';
+import {ControlsComponent} from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     EditorComponent,
-    HelpComponent,
     OutputComponent,
+    ControlsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent {}
+export class EditorComponent {
+  public readonly defaultCode: string = `
+foo <- 3 + 4 + (7 - 2)
+bar <- foo + 42
+print bar
+  `;
+}
